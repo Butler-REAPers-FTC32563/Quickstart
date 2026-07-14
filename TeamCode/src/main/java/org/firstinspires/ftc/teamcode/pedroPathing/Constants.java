322c1673f277bfb09708c8ff51398df543b16b10
Butler REAPers FTC32563 <butlerreaper@gmail.com>
2026-07-14 19:02:40 -0400

    Tuned everything and fixed the pinpot constants
    I tuned the X, Y, zero power accel's and the coffefients in panels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -13,10 +13,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(6.5);
+            .mass(6.5)
+            .forwardZeroPowerAcceleration(-38.57486443815851)
+            .lateralZeroPowerAcceleration(-58.71267221629335);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1)
+            .maxPower(0.7)
             .rightFrontMotorName("front_right_motor")
             .rightRearMotorName("back_right_motor")
             .leftRearMotorName("back_left_motor")
@@ -24,12 +26,14 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .xVelocity(42.68055112343135)
+            .yVelocity(36.5112540177473)
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(0)
-            .strafePodX(0)
-            .distanceUnit(DistanceUnit.MM)
+            .strafePodX(3.75)
+            .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
