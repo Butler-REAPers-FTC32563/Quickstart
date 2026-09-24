763f25c3d9a420b97fcc082bbab726e0a8588f1f
Butler REAPers FTC32563 <butlerreaper@gmail.com>
2026-09-24 17:48:01 -0400

    added a Auto program and fixed position of odometry pods

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPath.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.pedropathing.api.Paths.*;
+
+import com.pedropathing.api.PoseFactory;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.ivy.Command;
+import com.pedropathing.ivy.Scheduler;
+import static com.pedropathing.ivy.Scheduler.schedule;
+import static com.pedropathing.ivy.commands.Commands.*;
+import static com.pedropathing.ivy.groups.Groups.sequential;
+import static com.pedropathing.ivy.pedro.PedroCommands.follow;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.pedro.Constants;
+
+@Autonomous(name = "AutoPath", group = "Autonomous")
+public class AutoPath extends LinearOpMode {
+
+    private Follower follower;
+
+    private final PoseFactory poseFactory = PoseFactory.degrees();
+
+    private final Pose start = poseFactory.of(56, 8, 90);
+    private final Pose path1 = poseFactory.of(56, 36, 90);
+    private final Pose point2 = poseFactory.of(24, 36, 180);
+    private final Pose point3 = poseFactory.of(24, 108, 90);
+    private final Pose point4 = poseFactory.of(12, 108, 180);
+
+    // Autonomous routine
+    public Command autoRoutine() {
+        return sequential(
+                follow(follower, path1()),
+                follow(follower, path2()),
+                follow(follower, path3()),
+                follow(follower, path4())
+        );
+    }
+
+    @Override
+    public void runOpMode() {
+        Scheduler.reset();
+        follower = Constants.create(hardwareMap);
+        follower.setPose(start);
+        follower.update();
+
+        waitForStart();
+        schedule(autoRoutine());
+
+        while (opModeIsActive()) {
+            follower.update();
+            Scheduler.execute();
+
+            telemetry.addData("x", follower.pose().x());
+            telemetry.addData("y", follower.pose().y());
+            telemetry.addData("heading", follower.pose().heading());
+
+            if (follower.currentPath() != null) {
+                telemetry.addData("Current path distance remaining", follower.distanceToEndpoint());
+                telemetry.addData("Path number", follower.pathIndex());
+            }
+
+            telemetry.update();
+        }
+    }
+
+    public Path path1() {
+        return line(start, path1).constant(path1);
+    }
+
+    public Path path2() {
+        return line(path1, point2).constant(point2);
+    }
+
+    public Path path3() {
+        return line(point2, point3).constant(point3);
+    }
+
+    public Path path4() {
+        return line(point3, point4).constant(point4);
+    }
+}
